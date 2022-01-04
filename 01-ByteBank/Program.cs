af41b5a4cbdd4ecde49a94b12a0619ca8da1d51e
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-01-04 13:48:50 -0300

    criando o projeto

---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 3423;
+            contaDaGabriela.saldo = 100;
+        }
+    }
+}
---
+++ 02-ByteBank/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

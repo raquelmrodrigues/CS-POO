d32e71316901c6872581fa27bccb38ae795da309
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-01-05 14:10:22 -0300

    herança

---
+++ 08-ByteBank/GerenciadorBonificacao.cs
@@ -0,0 +1,24 @@
+﻿using _08_ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_ByteBank
+{
+    public class GerenciadorBonificacao
+    {
+        private double _totalBonificacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public double GetTotalBonificacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
---
+++ 08-ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using _08_ByteBank.Funcionarios;
+
+namespace _08_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+            
+            Funcionario carlos = new Funcionario();
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "28371283";
+            carlos.Salario = 2000;
+
+            gerenciador.Registrar(carlos);
+
+            Diretor Roberta = new Diretor();
+
+            carlos.Nome = "Roberta";
+            carlos.CPF = "2342341283";
+            carlos.Salario = 5000;
+
+            gerenciador.Registrar(Roberta);
+        }
+    }
+}

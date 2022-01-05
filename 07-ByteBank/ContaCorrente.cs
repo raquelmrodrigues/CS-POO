72c79019eaa31ee02bbdbd42f7729488729fac90
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-01-05 01:07:19 -0300

    Construtores e membros estáticos

---
+++ 07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,79 @@
+﻿
+
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+
+        public Cliente Titular { get; set; }
+
+        public static int TotalDeContasCriadas { get; set; }
+
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+
+        public Cliente Titular { get;  set; }
+
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+
+                }
+                else
+                {
+                    _saldo = value;
+                }
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
---
+++ 07-ByteBank/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta= new ContaCorrente(948, 2343);
+
+        }
+    }
+}
